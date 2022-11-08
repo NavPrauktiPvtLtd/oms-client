1281591f6ff1ed4e8314a8462caf8735f2dd4e6e
NavPrauktiPvtLtd <navprayuktipvtltd@gmail.com>
2022-11-08 16:03:02 +0530

    fixes

--- app/main.py
+++ app/main.py
@@ -1,13 +1,80 @@
 import subprocess
+import requests
 import paho.mqtt.client as mqtt
 import ast 
+import os
+import time
+from pathlib import Path
 from threading import Thread
 from logger.logger import setup_applevel_logger
+from dotenv import load_dotenv
+from player import Player
 
-UNQIUE_ID = "GQNQQRudtv2rfJG"
+
+load_dotenv()
 
 logger = setup_applevel_logger(__name__)
 
+BASE_DIR = str(Path(os.path.dirname(os.path.abspath(__file__))).parents[0])
+VIDEOS_DIR = os.path.join(BASE_DIR, "videos")
+
+def file_path(x): return os.path.join(VIDEOS_DIR, x)
+
+if not os.path.exists(VIDEOS_DIR):
+    os.makedirs(VIDEOS_DIR)
+
+SERIAL_NO = os.getenv('SERIAL_NO')
+
+if not SERIAL_NO:
+    logger.error('Serial no not found')
+    exit()
+
+NEW_PLAYER = None
+
+def start_app():
+    try:
+        host = "test.mosquitto.org"
+        client = mqtt.Client(SERIAL_NO)
+        client.connect(host=host)
+        client.on_connect= on_connect    
+        client.on_message= on_message
+        client.loop_forever()
+    except Exception as e:
+        logger.error(e)
+        time.sleep(1)
+        start_app() 
+
+def download_video_from_url(url,path):
+    r = requests.get(url)
+    with open(path, 'wb') as f:
+        f.write(r.content)
+
+def play_video(filepath):
+    # TODO: find a way to terminate the player
+    player = Player()
+    player.play([filepath])
+
+
+def play_video_handler(message):
+    data = str(message.payload.decode("utf-8"))
+    newData = ast.literal_eval(data)
+
+    name = newData['name']
+    url = newData['path']
+
+    # TODO: validate the dict 
+
+    filepath = file_path(name)
+
+    if os.path.exists(filepath):
+        logger.info('video already exists skipping download')
+        play_video(filepath)
+    else:
+        logger.info('downloading video')
+        download_video_from_url(url,filepath)
+        play_video(filepath)
+
+ 
 
 def display_url(url):
     logger.info(f'URL received: {url}')
@@ -32,16 +99,12 @@ def stop_media_handler():
     subprocess.call(["pkill", "firefox"])
     subprocess.call(["pkill", "mpv"])
 
-
-
-
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         logger.info("Connected to broker")
         client.subscribe("DISPLAY_URL")
         client.subscribe("STOP_MEDIA")
-
-
+        client.subscribe("PLAY_VIDEO")
     else:
         logger.error("Connection failed")
 
@@ -54,14 +117,14 @@ def on_message(client, userdata, message):
     if message.topic == "STOP_MEDIA":
         stop_media_handler()
 
-mqttBroker = "localhost"
+    if message.topic == "PLAY_VIDEO":
+        play_video_handler(message)
 
-client = mqtt.Client(UNQIUE_ID)
-client.on_connect= on_connect    
-client.on_message= on_message  
-client.connect(mqttBroker,1883)        
+
+start_app()
+           
       
 
-client.loop_forever()
+
 
 
--- app/player.py
+++ app/player.py
@@ -10,13 +10,14 @@ class Player:
 
         # media_player.fullscreen = True
         media_player.loop_playlist = True
-
+        
         for i in playlist:
             if not i :
                 continue
             media_player.playlist_append(i)
         media_player.playlist_pos = 0
         # media_player.wait_for_playback()
+        
         return media_player
 
 # media_player = vlc.MediaListPlayer()
