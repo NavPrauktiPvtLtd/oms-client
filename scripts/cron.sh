f02e4558e666ee6710b488d76fff30f675e8df9a
NavPrauktiPvtLtd <navprayuktipvtltd@gmail.com>
2023-07-14 13:46:58 +0530

    fix: scripts

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -7,11 +7,11 @@ log_dir=/home/pi/oms-client/scripts/logs
 chmod +x "/home/pi/oms-client/update.sh"
 
 # this will run every 30 minutes
-(sudo crontab -l | grep -v update.sh ; echo "*/30 * * * * /home/pi/oms-client/update.sh >> ${log_dir}/cron.log 2>&1") | crontab -
+(sudo crontab -l | grep -v update.sh ; echo "*/30 * * * * /home/pi/oms-client/update.sh >> /home/pi/oms-client/logs/cron.log 2>&1") | crontab -
 
 # cron job to delete log files
 
-chmod +x "${script_dir}/delete-log.sh"
+chmod +x "/home/pi/oms-client/scripts/delete-log.sh"
 
 # this will run every month
-(sudo crontab -l | grep -v delete-log ; echo "0 0 1 * * ${script_dir}/delete-log.sh >> ${log_dir}/cron.log 2>&1") | crontab -
+(sudo crontab -l | grep -v delete-log ; echo "0 0 1 * * /home/pi/oms-client/scripts/delete-log.sh >> /home/pi/oms-client/logs/cron.log 2>&1") | crontab -
